a21b34a573f5b7762fee585757d13c0534b3d630
Vasandr15 <vasandrey007@gmail.com>
2025-02-14 16:31:15 +0700

    eslint configured and stylelint

--- filmclub-frontend/.prettierrc.cjs
+++
@@ -1,7 +0,0 @@
-const { prettier } = require('@siberiacancode/prettier');
-
-/** @type {import('prettier').Config} */
-module.exports = {
-    ...prettier,
-    endOfLine: "lf"
-};
--- filmclub-frontend/vite.config.ts
+++ filmclub-frontend/vite.config.ts
@@ -1,14 +1,15 @@
-import path from "path"
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import * as path from 'path';
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-})
+  },
+});
